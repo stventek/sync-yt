cf8be3d7d93d5d27c98b506377845c53416d25ad
xenizs <xenizs@protonmail.com>
2022-12-17 12:59:42 -0400

    fix sqlite for dev

--- src/databases/database.ts
+++ src/databases/database.ts
@@ -3,7 +3,7 @@ import {Sequelize} from 'sequelize'
 const logging = process.env.ENV === 'PROD' ? false : console.log
 
 if(process.env.ENV == 'LOCAL')
-    var sequelize = new Sequelize('sqlite::memory:', {logging})
+    var sequelize = new Sequelize('sqlite::memory:', {logging, storage:'./db.sqlite'})
 else
     var sequelize = new Sequelize(process.env.DB_CONNECTION_STRING || '', {logging})
     
